cd0ab97ccb72fe0977a9f2a16fe15cdd359ea19c
fgostev <fgostev@gmail.com>
2023-08-03 10:25:25 -0600

    server add, created in index.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Learning project - introduction to node.js
+
+Goal of the project - launch 4 different pages on the server side with node.
--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("i work")
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    
+      if(req.url === '/'){
+        fs.readFile(
+          path.join(__dirname, 'public', 'index.html'), 
+            (err, content) => {
+              if(err) throw err;
+              res.writeHead(200, {'Content-Type':'text/html'})
+              res.end(content)
+        })
+      }
+
+      else if(req.url === '/about.html'){
+        fs.readFile(
+          path.join(__dirname, 'public', 'about.html'), 
+            (err, content) => {
+              if(err) throw err;
+              res.writeHead(200, {'Content-Type':'text/html'})
+              res.end(content)
+        })
+      }
+
+      else if(req.url === '/contact-me.html'){
+        fs.readFile(
+          path.join(__dirname, 'public', 'contact-me.html'), 
+            (err, content) => {
+              if(err) throw err;
+              res.writeHead(200, {'Content-Type':'text/html'})
+              res.end(content)
+        })
+      }
+
+      else{
+        fs.readFile(
+            path.join(__dirname, 'public', '404.html'), 
+              (err, content) => {
+                if(err) throw err;
+                res.writeHead(200, {'Content-Type':'text/html'})
+                res.end(content)
+          })
+        }
+    });
+
+
+const PORT = process.env.PORT || 5550;
+server.listen(PORT, () => console.log(`Server running on ${PORT}`))
